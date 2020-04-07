7f02091411c6c8f40194e050e795c710089af755
Irina Plaksina <irene.plaksina@gmail.com>
2020-04-06 21:54:55 -0400

    Reveal question with list of answers

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 //DOM dependencies
 let timerEl = document.querySelector("#counter");
 let starterEl = document.querySelector("#welcome-page");
+let questionWrapperEl = document.querySelector("#questionWrapper");
 let questionEl = document.querySelector("#question");
+let answersEl = document.querySelector("#answers");
+let answerItemEl = document.querySelector("#answers > li > button");
+//let answerItemEl = document.querySelectorAll("#answers" > "li");
+console.log(`answerItemEl: ${answerItemEl}`);
+
 
 //questions
 let questions = {
@@ -17,7 +23,7 @@ let questions = {
     // {q: "", a: []},
     // {q: "", a: []},
 }
-console.log(questions.question1);
+console.log(questions.question1.a[0]);
 //start countdown when user clicks Start Quiz button
 function countDown() {
     let count = 75;
@@ -34,7 +40,16 @@ function countDown() {
 //display question when user clicks the button
 function askQuestion() {
 starterEl.classList.add("hide");
-questionEl.classList.remove("hide");
+questionWrapperEl.classList.remove("hide");
+let list = questions.question1.a;
+questionEl.textContent = questions.question1.q;
+for (let i = 0; i < list.length; i++){
+    console.log("listitem: " + list[i]);
+    answerItemEl.textContent = list[i];
+    console.log(answerItemEl);
+    
+}
+
 
 }
 
