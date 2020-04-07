195729e39475bc38dfb7ba2e38460003c19418a1
Irina Plaksina <irene.plaksina@gmail.com>
2020-04-06 21:11:38 -0400

    Added questions to JS

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
 //DOM dependencies
-let timer = document.querySelector("#counter");
-//start countdown when users clicks Start Quiz button
+let timerEl = document.querySelector("#counter");
+let starterEl = document.querySelector("#welcome-page");
+let questionEl = document.querySelector("#question");
 
+//questions
+let questions = {
+    question1: {
+        q: "Commonly used data types DO NOT include:",
+        a: ["strings", "booleans", "alerts", "numbers"]
+    },
+    question2: {
+        q: "The condition in an if/else statement is enclosed within ___.",
+        a: ["quotes", "curly brackets", "parentheses", "square brackets"]
+    },
+    // {q: "", a: []},
+    // {q: "", a: []},
+    // {q: "", a: []},
+}
+console.log(questions.question1);
+//start countdown when user clicks Start Quiz button
 function countDown() {
     let count = 75;
-
     let countInterval = setInterval(function () {
-        timer.textContent = count;
+        timerEl.textContent = count;
+        //console.log(count);
         count--;
         if (count < 0) {
             clearInterval(countInterval);
         }
     }, 1000);
-    
-    console.log(count);
 }
-//display question when users clicks the button
+
+//display question when user clicks the button
+function askQuestion() {
+starterEl.classList.add("hide");
+questionEl.classList.remove("hide");
+
+}
+
 //verify the answer: show the result
 //if the answer is incorrect substract 10 seconds from the remaining countdown
 //show final page when all questions are answered / time is up
